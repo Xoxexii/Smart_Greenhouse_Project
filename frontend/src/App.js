22e3023e5c9d97afb348ff590c6a5955a3113ba8
Xoxexii <104296765+Xoxexii@users.noreply.github.com>
2023-05-26 14:11:33 +0700

    Update Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -206,7 +206,7 @@ useEffect(()=>{
 return (
   <div className="App">
     <div className='Farm_name'>
-      Smart GreenFarm
+      Smart GreenFarmJaidee
     </div>
     <div className="Temp_Data_area">
       <Temp_data  A={A} E={E} />
